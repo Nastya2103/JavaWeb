f7b78b2dba4bb48d4836418795c8ed96ae909b56
Nastya2103 <korobeynikova2103@mail.ru>
2023-03-13 16:22:22 +0300

    Практическая работа 7

---
+++ src/main/java/Lesson6/MainPage.java
@@ -0,0 +1,58 @@
+package Lesson6;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MainPage extends BaseView {
+    public MainPage(WebDriver driver) {
+        super(driver);
+        productBlock = new ProductBlock(driver);
+    }
+
+    @FindBy (xpath=".//*[@class=\"menu-item\"]")
+    public WebElement signInButton;
+
+    @Step("Регистрация")
+    public LoginPage clickSignInButton() {
+        signInButton.click();
+        return new LoginPage(driver);
+    }
+
+    @FindBy (xpath = "//*[@href=\"/ru/mind-map-pricing\"]")
+    public WebElement priceInformationMap;
+
+    @Step("Клик на кнопку открытия информации о тарифах")
+    public PricesPage clickPriceInformationMap() {
+        priceInformationMap.click();
+        return new PricesPage(driver);
+    }
+
+    @FindBy(xpath = ".//*[@id=\"close-notification-banner\"]")
+    public WebElement closeNotificationBanner;
+
+    @FindBy(xpath = ".//*[@id=\"cb-btn-ok\"]")
+    public WebElement cookieOkButton;
+
+    @Step("Закрываем кукисы")
+    public MainPage clickCookies() {
+        cookieOkButton.click();
+        return this;
+    }
+
+    @FindBy (xpath = "//*[@id=\"btn-product-dropdown\"]//*[@class=\"link\"]")
+    public WebElement productButton;
+
+    @Step("Открытие меню продукции")
+    public ProductBlock clickProductButton() {
+
+        productButton.click();
+        return new ProductBlock(driver);
+    }
+
+    public ProductBlock productBlock;
+
+
+}
+
